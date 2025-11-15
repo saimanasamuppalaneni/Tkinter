ec5ee307ff378e8b50b252ede426d3c0b756e2d4
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-11-15 18:04:48 +0530

    2

---
+++ class 2 tkinter geometry managers/login.py
@@ -0,0 +1,28 @@
+from tkinter import *
+window = Tk()
+window.title("Login screen")
+window.geometry("400x400")
+window.configure(bg = 'grey')
+frame1 = Frame(window,relief = 'ridge',bg = 'blue',width = 300,height = 150,bd = 3)
+frame1.pack()
+def greet():
+    name= entry1.get()
+    greeting = "welcome" +name +"\n login successful!"
+    text1.delete("1.0","end")
+    text1.insert("1.0",greeting)
+label1 = Label(frame1,text = "username" ,bg = 'blue',fg = 'white')
+label2 = Label(frame1,text = "Password" ,bg = 'blue',fg = 'white')
+entry1 = Entry(frame1)
+entry2 = Entry(frame1,show = "*")
+button1 = Button(frame1,text ="login",bg = 'light blue',command = greet)
+
+label1.place(x = 10,y = 10)
+label2.place(x = 10,y = 40)
+entry1.place(x = 100,y = 10)
+entry2.place(x = 100,y = 40)
+button1.place(x = 75,y = 80)
+
+text1 = Text(window,relief = 'groove',bd=3,bg = 'skyblue')
+text1.pack(pady = 10)       
+
+root.mainloop()
